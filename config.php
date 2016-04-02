ab948b3f16c701a19819e42efeb0744738fffe67
Averil <averil.case@gmail.com>
2016-04-02 18:41:45 +1100

    test

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// database login parameters
-define('DBHOST','localhost'); // host
-define('DBUSER','root'); // username
-define('DBPASS','Charlie913'); // password
-define('DBNAME','excel'); //database name
-
-?>
